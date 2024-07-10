51260c27a01af3f158efe1d6ec9a80b6bd435879
kume1a <kumela011@gmail.com>
2024-07-10 19:13:35 +0400

    igservice api

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ lint:
 	pylint --disable=R,C,W1203,W0702 app.py
 
 run:
-	python app.py
+	python src/app.py
 
 protogen:
-	python -m grpc_tools.protoc --proto_path=proto proto/*.proto --python_out=proto --grpc_python_out=proto
+	python -m grpc_tools.protoc --proto_path=src src/*.proto --python_out=src --grpc_python_out=src
---
+++ src/app.py
@@ -0,0 +1,74 @@
+import grpc
+import urllib.request
+import time
+
+from pathlib import Path
+from instagrapi import Client
+from concurrent import futures
+
+import igapi_pb2
+import igapi_pb2_grpc
+
+class IGAPIServicer(igapi_pb2_grpc.IGAPIServicer):
+    def CreateIGTVVideo(self, request, context):
+        ig_username = request.igUsername
+        ig_password = request.igPassword
+        video_url = request.videoURL
+        title = request.title
+        caption = request.caption
+        thumbnail_url = request.thumbnailURL
+
+        if not ig_username or not ig_password or not video_url \
+           or not title or not caption or not thumbnail_url:
+          print('Invalid request, missing parameters')
+
+          context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+          context.set_details('Missing parameters')
+
+          return igapi_pb2.CreateIGTVVideoResponse()
+
+        try:
+          video_path = f'upload/{time.time()}.mp4'
+          thumbnail_path = f'upload/{time.time()}.jpg'
+
+          urllib.request.urlretrieve(request.videoURL, video_path)
+          urllib.request.urlretrieve(request.thumbnailURL,  thumbnail_path)
+
+          cl = Client()
+          success = cl.login(ig_username, ig_password)
+
+          if not success:
+            print('Invalid ig credentials, login failed')
+
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details('Invalid ig credentials')
+
+            return igapi_pb2.CreateIGTVVideoResponse()
+          
+          media = cl.igtv_upload(
+            Path(video_path), 
+            request.title, 
+            request.caption,
+            Path(thumbnail_path)
+          )
+
+          print('Media uploaded successfully!')
+          print(media.id)
+
+          return igapi_pb2.CreateIGTVVideoResponse(id=media.id)
+        except Exception as e:
+           print(e)
+
+           context.set_code(grpc.StatusCode.INTERNAL)
+           context.set_details(str(e))
+
+           return igapi_pb2.CreateIGTVVideoResponse()
+
+if __name__ == "__main__":
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+
+    igapi_pb2_grpc.add_IGAPIServicer_to_server(IGAPIServicer(), server)
+
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
